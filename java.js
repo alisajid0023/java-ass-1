4a6bf88c23109a6b7f16c77df3e7222837a2b4d4
alisajid0023 <155763126+alisajid0023@users.noreply.github.com>
2024-01-24 15:27:10 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+// Assignment1
+
+var age = "18 year old"
+var isStudent = " Yes i'm becholors student"
+console.log(age);
+console.log(isStudent);
+alert("Welcome to my portfolio. My name is Ali Sajid and i'm frontend developer")
+// Assignment1
+
+
